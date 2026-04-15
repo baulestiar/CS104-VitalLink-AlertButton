2308ead7e018ca4299c0fb581243179f175d6a73
baulestiar <b.aulestia.r@gmail.com>
2026-04-15 00:28:01 -0600

    Resolve merge conflict in alert_button.py

--- alert_button.py
+++ alert_button.py
@@ -1,13 +1,30 @@
 import time
 import RPi.GPIO as GPIO
+import requests
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+def send_telegram_message():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": "Someone pressed the alert button!"
+    }
+    requests.post(url, json=data)
+
 button_pressed = False
+
 while True:
     if GPIO.input(7) == GPIO.HIGH and not button_pressed:
         print("Someone pressed the alert button!")
+        send_telegram_message()
         button_pressed = True
+
     elif GPIO.input(7) == GPIO.LOW:
         button_pressed = False
+
     time.sleep(0.1)
